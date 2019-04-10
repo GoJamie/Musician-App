5249374af3e64f6df0945e0e3c5502dede32fa83
niittyviita <saniitty@student.oulu.fi>
2019-04-10 16:03:44 +0300

    message template

--- app/src/main/java/com/example/musicians/Message.java
+++ app/src/main/java/com/example/musicians/Message.java
@@ -5,7 +5,7 @@ public class Message {
     String message;
 
 
-    message_structure(String username, String message) {
+    Message(String username, String message) {
         this.username = username; // The username of the event.
         this.message = message; // The message body: actual message.
     }
