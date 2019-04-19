0d5bc70b1ab783ce63e309e759ae4336dfe545d3
gojamie <wangbangju@outlook.com>
2019-04-19 15:49:14 +0300

    delete unused attribute

--- app/src/main/java/com/example/musicians/Event.java
+++ app/src/main/java/com/example/musicians/Event.java
@@ -1,7 +1,5 @@
 package com.example.musicians;
 
-import java.util.Date;
-
 public class Event extends EventList {
     String name;
     String time;
@@ -9,15 +7,13 @@ public class Event extends EventList {
     String owner;
     String city;
     String address;
-    String private_public;
 
-    Event(String name, String city, String address, String time, int participants, String owner, String private_public) {
+    Event(String name, String city, String address, String time, int participants, String owner) {
         this.name = name; // The name of the event
         this.city = city; // The city of the event
         this.address = address; // The address of the event
         this.time = time; // The time of the event
         this.participants = participants; // The number of participants to the event
         this.owner = owner; // The person who has created the event
-        this.private_public = private_public; // Whether the event is private or public
     }
 }
