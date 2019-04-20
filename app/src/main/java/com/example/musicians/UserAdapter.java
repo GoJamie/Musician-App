e6f3ac791f189d73551a00cf587524e6c534f594
niittyviita <saniitty@student.oulu.fi>
2019-04-20 14:47:21 +0300

    created user stuffs

--- app/src/main/java/com/example/musicians/UserAdapter.java
+++ app/src/main/java/com/example/musicians/UserAdapter.java
@@ -1,4 +1,54 @@
 package com.example.musicians;
 
-public class UserAdapter {
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+public class UserAdapter extends RecyclerView.Adapter<UserAdapter.UserViewHolder> {
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+
+        CardView user_card;
+        TextView UserName;
+
+
+        UserViewHolder(View userView) {
+            super(userView);
+            user_card = userView.findViewById(R.id.user_card);
+            UserName = userView.findViewById(R.id.user_name);
+
+        }
+    }
+
+    UserAdapter(List<User> Users){
+        this.Users = Users;
+    }
+
+    @Override
+    public void onAttachedToRecyclerView(RecyclerView recyclerView) {
+        super.onAttachedToRecyclerView(recyclerView);
+    }
+
+    @Override
+    public UserViewHolder onCreateViewHolder(ViewGroup viewGroup, int i) {
+        View user_view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.user_template, viewGroup, false);
+        UserViewHolder pvh = new UserViewHolder(user_view);
+        return pvh;
+    }
+
+    @Override
+    public void onBindViewHolder(UserViewHolder UserViewHolder, int i) {
+        // TODO: Add correct user info
+        UserViewHolder.UserName.setText(Users.get(i).getName());
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return Users.size();
+    }
 }
--- app/src/main/java/com/example/musicians/UserList.java
+++ app/src/main/java/com/example/musicians/UserList.java
@@ -1,6 +1,58 @@
 package com.example.musicians;
 
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.View;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
 
 public class UserList extends AppCompatActivity {
+
+    private RecyclerView user_recycler;
+    private static final String TAG = "UserList";
+    Context context;
+    private List<User> users = new ArrayList<>();;
+    FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.user_list);
+        user_recycler = findViewById(R.id.user_recycler);
+        LinearLayoutManager user_manager = new LinearLayoutManager(this);
+        user_recycler.setLayoutManager(user_manager);
+        //initializeData();
+        // Add a new document with a generated ID
+        user_recycler.addOnItemTouchListener(
+                new UserRecyclerItemClickListener(context, user_recycler ,new UserRecyclerItemClickListener.OnItemClickListener() {
+                    @Override public void onItemClick(View view, int position) {
+                        //
+                        startActivity(new Intent(UserList.this, Dashboard.class));
+                    }
+
+                })
+        );
+
+    }
+
+
+    private void initializeAdapter(){
+        RecyclerView.Adapter user_adapter = new UserAdapter(users);
+        user_recycler.setAdapter(user_adapter);
+    }
 }
